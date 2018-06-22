2d65aca074ebc98cb5e0fdbe8478b306ada604c8
aleph1507 <xrristo@gmail.com>
2018-06-22 20:40:39 +0200

    session00001

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var express = require('express'),
     app = express(),
-    path = require('path');
+    path = require('path'),
+    cookieParser = require('cookie-parser'),
+    session = require('express-session');
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('hogan-express'));
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public'))); // ./public
+app.use(cookieParser());
+app.use(session( { secret:'catscanfly', saveUninitialized:true, resave:true } ))
 
 require('./routes/routes.js')(express, app);
 
@@ -17,3 +21,8 @@ require('./routes/routes.js')(express, app);
 app.listen(3000, function(){
   console.log('ChatCAT working on port 3000');
 });
+
+
+// mongolab user chatcat pass: chatcat1
+
+// mongodb://chatcat:chatcat1@ds161740.mlab.com:61740/chat_cat
--- routes/routes.js
+++ routes/routes.js
@@ -9,5 +9,14 @@ module.exports = function(express, app) {
     res.render('chatrooms', { title: 'Chatrooms' });
   });
 
+  router.get('/setcolor', function(req, res, next){
+    req.session.favColor = 'red';
+    res.send('setting favorite color');
+  });
+
+  router.get('/getcolor', function(req, res, next){
+    res.send('Favourite color: ' + (req.session.favColor===undefined ? 'not found' : req.session.favColor));
+  });
+
   app.use('/', router);
 }
